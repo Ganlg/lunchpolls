5a5012e78dcd145a624e8493d0c9b8229cca69bf
Gan <liugannankai@gmail.com>
2016-09-25 16:07:29 -0500

    Turn off debug mode

--- LunchPoll/settings.py
+++ LunchPoll/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!bbqgi--!9-kwhxcn4x&)s2dxg#z7y(140ol11+j0cy8q@o^^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fedexlunch.pythonanywhere.com']
 
 
 # Application definition
